ee2d6bd58890cf63455305d88e3ef8db7e615d2d
andres gonzalez <miroenlared@gmail.com>
2016-03-25 19:17:26 -0500

    avance de maquetacion proyectos inicio de las modales

--- js/script.js
+++ js/script.js
@@ -12,35 +12,84 @@ $(document).ready(function() {
   }, function() {
     $(this).removeClass('hvr-shrink');
   });
-
+  $('.btnTop').hover(function() {
+    $(this).addClass('animated tada infinite');
+  }, function() {
+    $(this).removeClass('animated tada infinite');
+  });
   //
   setTimeout("$('h1').show()", 500);
   setTimeout("$('h2').show()", 1000);
   setTimeout("$('.wrapNavHome').show()", 1500);
   setTimeout("$('.downButton').css('display', 'block').addClass('animated fadeInUp')", 2500);
 
-//
+// scroll down animacion
 
         $(window).scroll(function(){
-            if ($(this).scrollTop() > 800) {
+
+
+
+            if ($(this).scrollTop() > 400) {
                 //$('header').fadeIn(500);
-                $('header').show().addClass('animated bounceInUp')
+                $('header').show().addClass('animated bounceInDown');
+                $('.btnTop').css('display', 'none');
+
             } else {
                 $('header').fadeOut(500);
             }
+
+            // animacion home
+            if ($(this).scrollTop() === 0){
+              $('.cont01 .container').show();
+              setTimeout("$('h1').show()", 500);
+              setTimeout("$('h2').show()", 1000);
+              setTimeout("$('.wrapNavHome').show()", 1500);
+              //setTimeout("$('.downButton').css('display', 'block').addClass('animated fadeInUp')", 2500);
+            }
+            // animacion iconos
+            if($(this).scrollTop() > 800){
+              $('.wrapIcon img').css('display','block');
+              $('.wrapIcon').eq(0).find('img').addClass('animated rotateInDownLeft');
+              $('.wrapIcon').eq(1).find('img').addClass('animated rotateIn');
+              $('.wrapIcon').eq(2).find('img').addClass('animated rotateInDownRight');
+              $('.cont01 .container').fadeOut(800);
+              $('.btnHab').addClass('active');
+              $('.btnPro, .btnAce').removeClass('active');
+              $('.btnTop').css('display', 'block');
+            }
+            else{
+              $('.wrapIcon img').fadeOut(800);
+            }
+            if($(this).scrollTop() > 1600){
+              $('.btnPro').addClass('active');
+              $('.btnHab, .btnAce').removeClass('active');
+            }
+
+
         });
 
-//
+//botones scroll
+        $('.btnTop').click(function(event) {
+          $('html, body').animate({
+              scrollTop: 0
+          }, 200);
+        });
 
-        $('.downButton').click(function() {
-
+        $('.downButton, .homeHab, .btnHab').click(function() {
                 event.preventDefault();
-
                 $('html, body').animate({
                     scrollTop: 810
                 }, 400);
-
-
         });
+        $('.homePro, .btnPro').click(function(event) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: 1610
+            }, 400);
+        });
+
+// hover iconos
+
+
 
 });
