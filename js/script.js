b627be412aa69ee0f4b7e317be4409be23ac46b4
Andrés González <miroenlared@gmail.com>
2016-03-28 08:53:50 -0500

    ajuste pagina inicial y scroll navigation

--- js/script.js
+++ js/script.js
@@ -23,6 +23,62 @@ $(document).ready(function() {
   setTimeout("$('.wrapNavHome').show()", 1500);
   setTimeout("$('.downButton').css('display', 'block').addClass('animated fadeInUp')", 2500);
 
+  //botones scroll
+
+        var divScroll = "top";
+
+          $('.btnTop').click(function(event) {
+                  divScroll = "top";
+                  console.log( divScroll );
+            $('html, body').animate({
+                 scrollTop: $(".cont01 ").offset().top
+            }, 200);
+          });
+
+
+          $('.downButton, .homeHab, .btnHab').click(function() {
+                  event.preventDefault();
+                  divScroll = "hab"
+                  console.log( divScroll );
+                  $('html, body').animate({
+                      scrollTop: $(".cont02 ").offset().top
+                  }, 400);
+          });
+
+
+          $('.homePro, .btnPro').click(function(event) {
+              event.preventDefault();
+              $('html, body').animate({
+                  scrollTop: $(".cont03 ").offset().top
+              }, 400);
+          });
+
+
+
+  /*Funcion para detectar el tamaño del navegador*/
+var fullwidth = $(window).width();
+var fullheight = $(window).height();
+
+
+$(document).ready(function() {
+	$(".cont01").height(fullheight);
+});
+
+$(window).resize(function() {
+	var newWidth = $(window).width();
+	var newHeight = $(window).height();
+	if (newWidth != fullwidth || newHeight != fullheight) {
+		fullwidth = newWidth;
+		fullheight = newHeight;
+		doneResizing();
+	}
+});
+
+function doneResizing() {
+	//Ejecuta lo que sea despues de cambiar tamaño del navegador
+	$(".cont01").height(fullheight);
+}
+
 // scroll down animacion
 
         $(window).scroll(function(){
@@ -39,7 +95,8 @@ $(document).ready(function() {
             }
 
             // animacion home
-            if ($(this).scrollTop() === 0){
+           // if ($(this).scrollTop() === 0){
+            if ( divScroll == "top" || $(this).scrollTop() === 0 ){
               $('.cont01 .container').show();
               setTimeout("$('h1').show()", 500);
               setTimeout("$('h2').show()", 1000);
@@ -47,16 +104,18 @@ $(document).ready(function() {
               //setTimeout("$('.downButton').css('display', 'block').addClass('animated fadeInUp')", 2500);
             }
             // animacion iconos
-            if($(this).scrollTop() > 800){
+            //if($(this).scrollTop() > 800){
+            if( divScroll == "hab" ||  $(this).scrollTop() > fullheight ){
               $('.wrapIcon img').css('display','block');
               $('.wrapIcon').eq(0).find('img').addClass('animated rotateInDownLeft');
               $('.wrapIcon').eq(1).find('img').addClass('animated rotateIn');
               $('.wrapIcon').eq(2).find('img').addClass('animated rotateInDownRight');
-              $('.cont01 .container').fadeOut(800);
+              //$('.cont01 .container').fadeOut(800);
               $('.btnHab').addClass('active');
               $('.btnPro, .btnAce').removeClass('active');
               $('.btnTop').css('display', 'block');
             }
+
             else{
               $('.wrapIcon img').fadeOut(800);
             }
@@ -68,25 +127,6 @@ $(document).ready(function() {
 
         });
 
-//botones scroll
-        $('.btnTop').click(function(event) {
-          $('html, body').animate({
-              scrollTop: 0
-          }, 200);
-        });
-
-        $('.downButton, .homeHab, .btnHab').click(function() {
-                event.preventDefault();
-                $('html, body').animate({
-                    scrollTop: 810
-                }, 400);
-        });
-        $('.homePro, .btnPro').click(function(event) {
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: 1610
-            }, 400);
-        });
 
 // modales
 
