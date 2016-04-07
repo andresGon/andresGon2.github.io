20b38d084b2d11d2b6bacc73f8bffe5f2983717a
Andrés González <miroenlared@gmail.com>
2016-04-07 16:43:54 -0500

    dectectar sistema operativo movil

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+/**
+ * Determine the mobile operating system.
+ * This function either returns 'iOS', 'Android' or 'unknown'
+ *
+ * @returns {String}
+ */
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
+  {
+    return 'iOS';
+     alert('ios');
+  }
+  else if( userAgent.match( /Android/i ) )
+  {
+
+    //return 'Android';
+    alert('android');
+  }
+  else
+  {
+    return 'unknown';
+  }
+}
+
+
 $(document).ready(function() {
   // add class nav home
   $('.homeNav a').hover(function() {
