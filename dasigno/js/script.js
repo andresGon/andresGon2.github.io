e99eecd08ccb4e4083769231c46ef803d9f3316f
andres gonzalez <miroenlared@gmail.com>
2016-04-28 21:25:17 -0500

    slider de champeta

--- dasigno/js/script.js
+++ dasigno/js/script.js
@@ -1,5 +1,12 @@
 
   jQuery(document).ready(function($) {
-    $('.my-slider').unslider();
+    $('.sli-Lessons').unslider();
     $('.my-slider2').unslider();
   });
+
+$(document).ready(function() {
+  $('.unslider-nav').find('li').eq(0).html('Salsa');
+  $('.unslider-nav').find('li').eq(1).html('Champeta');
+  $('.unslider-nav').find('li').eq(2).html('Raggeaton');
+  $('.unslider-nav').find('li').eq(3).html('Vallenato');
+});
